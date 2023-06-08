b4d41c31601c66500110246cd5393aae0d77b143
ggeorgiu <ggeorgiu@uw.co.uk>
2023-06-08 17:26:52 +0300

    Add solution for generate parantheses

--- stack/generate-parentheses/main.go
+++ stack/generate-parentheses/main.go
@@ -5,35 +5,34 @@ import (
 	"strings"
 )
 
-var stack []string
-var res []string
-
 func main() {
 	fmt.Println(generateParenthesis(3))
 }
 
 func generateParenthesis(n int) []string {
-	stack = []string{}
-	res = []string{}
+	var stack []string
+	var res []string
+	var back func(int, int)
 
-	back(0, 0, n)
+	back = func(open int, closed int) {
+		if open == n && closed == n {
+			res = append(res, strings.Join(stack, ""))
+			return
+		}
+
+		if open < n {
+			stack = append(stack, "(")
+			back(open+1, closed)
+			stack = stack[:len(stack)-1]
+		}
+		if closed < open {
+			stack = append(stack, ")")
+			back(open, closed+1)
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	back(0, 0)
+
 	return res
-}
-
-func back(open, closed, n int) {
-	if open == n && closed == n {
-		res = append(res, strings.Join(stack, ""))
-		return
-	}
-
-	if open < n {
-		stack = append(stack, "(")
-		back(open+1, closed, n)
-		stack = stack[:len(stack)-1]
-	}
-	if closed < open {
-		stack = append(stack, ")")
-		back(open, closed+1, n)
-		stack = stack[:len(stack)-1]
-	}
 }
